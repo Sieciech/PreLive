16314eccb6e41b4ff4de824f9847ef9fb27c64d7
Sieciech <michal@freev.net>
2014-08-22 10:50:10 +0200

    Update notify.js

--- notify.js
+++ notify.js
@@ -64,41 +64,6 @@ function blinkTitle()
   }, 3000);
 };
 function newUpdate() {
-	var liveopts_str = "";
-	var groups_str = "";
-	var group_ignore = 0;
-	var timezone = "0";
-	var pretimezone = 0;
-	
-	if (liveopts_str.length > 0) {
-		var liveopts = $.trim(liveopts_str).split('|');
-		$.each(liveopts, function(index, value) { 
-			var cell = $('tr', '#filter_tbl').children().filter(function(){ return $(this).text() == value; });
-			if (cell) {
-				cell.css('color','white');
-			}
-		});
-	} else {
-		var liveopts = new Array();
-	}
-	
-	if (groups_str.length > 0) {
-		var groupopts = $.trim(groups_str).split('|');
-		$.each(groupopts, function(index, value) { 
-			if (value.length > 0) {
-				$('#group_list', '#filter_tbl').append('<option opt="group">'+value+'</option>');
-			}
-		});
-	} else {
-		var groupopts = new Array();
-	}
-	
-	if (group_ignore) {
-		$("td[opt='group_ignore']", '#filter_tbl').css('color','white');
-	} else {
-		$("td[opt='group_ignore']", '#filter_tbl').css('color','');
-	}
-
 	var ts = new Date().getTime();
 	$.get("lastpred.php", {
 		ts: ts,
@@ -141,7 +106,40 @@ function newUpdate() {
 		window.origSetTimeout(newUpdate, 1000);
 	});
 };
+var liveopts_str = "";
+var groups_str = "";
+var group_ignore = 0;
+var timezone = "0";
+var pretimezone = 0;
 
+if (liveopts_str.length > 0) {
+	var liveopts = $.trim(liveopts_str).split('|');
+	$.each(liveopts, function(index, value) { 
+		var cell = $('tr', '#filter_tbl').children().filter(function(){ return $(this).text() == value; });
+		if (cell) {
+			cell.css('color','white');
+		}
+	});
+} else {
+	var liveopts = new Array();
+}
+
+if (groups_str.length > 0) {
+	var groupopts = $.trim(groups_str).split('|');
+	$.each(groupopts, function(index, value) { 
+		if (value.length > 0) {
+			$('#group_list', '#filter_tbl').append('<option opt="group">'+value+'</option>');
+		}
+	});
+} else {
+	var groupopts = new Array();
+}
+
+if (group_ignore) {
+	$("td[opt='group_ignore']", '#filter_tbl').css('color','white');
+} else {
+	$("td[opt='group_ignore']", '#filter_tbl').css('color','');
+}
 window.origSetTimeout = window.setTimeout;
 window.setTimeouts = [];
 window.setTimeout = function(func, time)
@@ -161,6 +159,81 @@ window.setTimeout = function(func, time)
   }
 };
 console.log('Live Pre Notifier started');
-console.log(window, top);
 setTimeout(newUpdate, 400);
+$("td[opt='type']", '#filter_tbl').click(function() {
+	var celltext = $(this).text();
+	if (celltext.length > 0) {
+		if ($(this).attr('style')) {		
+			var getindex = $.inArray(celltext,liveopts);
+			if (getindex != -1) {
+				liveopts.splice(getindex, 1);
+			}
+			$(this).css('color','');
+		} else {
+			if ($.inArray(celltext,liveopts) == -1) {
+				liveopts.push(celltext);
+			}
+			$(this).css('color','white');
+			
+		}
+	}
+});
+
+$("td[opt='group_ignore']", '#filter_tbl').click(function() {
+	if ($(this).attr('style')) {		
+		group_ignore = 0;
+		$(this).css('color','');
+	} else {
+		group_ignore = 1;
+		$(this).css('color','white');
+		
+	}
+});
+
+$('#group', '#filter_tbl').keypress(function(event) {
+	if (event.which == '13') {
+		var text = $(this).val();
+		if (text.length > 0) {
+			if ($.inArray(text,groupopts) == -1) {
+				groupopts.push(text);
+				$('#group_list', '#filter_tbl').append('<option opt="group">'+text+'</option>');
+			}
+		}
+		$(this).val('');
+	}
+});
+
+$('#group_list').delegate("option[opt]", "click", function(){
+	var getindex = $.inArray($(this).val(),groupopts);
+	if (getindex != -1) {
+		groupopts.splice(getindex, 1);
+		$(this).remove();
+	}
+});
+
+$('input[name=save]', '#filter_tbl').click(function() {;
+	savebtn = $(this);
+	savebtn.css('outline','');
+	$.post('user.php', { mode: 'take_user', types: liveopts.join('|'), 'groups': groupopts.join('|'), group_ignore: group_ignore, done: 1, ext: 1 }, function(data) {
+		if (data == '1') {
+			savebtn.css('outline','#4CC417 solid thin');
+		} else {
+			savebtn.css('outline','red solid thin');
+		}
+	});
+});
+
+$("#user", "#menu").prettyPhoto({allow_resize: true, theme: 'facebook', keyboard_shortcuts: false, overlay_gallery: false, default_height: 400 });
+
+$('#livetable').ajaxStop(function(){
+	$('#livetable > table > tbody td[title]').qtip({
+		content : { text: false },
+		position: { target: 'mouse', adjust: { mouse: true, x: 10, y: 10 } },
+		show: { delay: 500, solo: true },
+		hide: { fixed: true },
+		style: { 'font-family': 'calibri', width: { max: 1000 } }
+	});
+	//$('#livetable > table > tbody td[title]').qtip({ content: { text: false }, position: { target: "mouse", adjust: { mouse: true, x: 10, y: 10 } }, show: { delay: 500, solo: true }, hide: { fixed: true }});
+});
+
 
