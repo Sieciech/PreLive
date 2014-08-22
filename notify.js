54a99557f14f36f658d9020107b74fb6bc6f6fc9
Sieciech <michal@freev.net>
2014-08-22 11:00:37 +0200

    Update notify.js

--- notify.js
+++ notify.js
@@ -64,63 +64,6 @@ function blinkTitle()
   }, 3000);
 };
 function newUpdate() {
-
-
-
-
-	window.liveopts_str = "";
-	window.groups_str = "";
-	window.group_ignore = 0;
-	window.timezone = "0";
-	window.pretimezone = 0;
-	
-	if (liveopts_str.length > 0) {
-		var liveopts = $.trim(liveopts_str).split('|');
-		$.each(liveopts, function(index, value) { 
-			var cell = $('tr', '#filter_tbl').children().filter(function(){ return $(this).text() == value; });
-			if (cell) {
-				cell.css('color','white');
-			}
-		});
-	} else {
-		var liveopts = new Array();
-	}
-	
-	if (groups_str.length > 0) {
-		var groupopts = $.trim(groups_str).split('|');
-		$.each(groupopts, function(index, value) { 
-			if (value.length > 0) {
-				$('#group_list', '#filter_tbl').append('<option opt="group">'+value+'</option>');
-			}
-		});
-	} else {
-		var groupopts = new Array();
-	}
-	
-	if (group_ignore) {
-		$("td[opt='group_ignore']", '#filter_tbl').css('color','white');
-	} else {
-		$("td[opt='group_ignore']", '#filter_tbl').css('color','');
-	}
-	window.origSetTimeout = window.setTimeout;
-	window.setTimeouts = [];
-	window.setTimeout = function(func, time)
-	{
-	  var fstr = func.toString();
-	  var updater = fstr.match(/window\.setTimeout\(update,\s/i)?true:false;
-	  //console.log('func', updater);
-	
-	  if(!updater)
-	  {
-	    var i = window.origSetTimeout(func, time);
-	    window.setTimeouts.push({
-	      i: i,
-	      func: func,
-	      time: time,
-	    });
-	  }
-	};
-
 	var ts = new Date().getTime();
 	$.get("lastpred.php", {
 		ts: ts,
@@ -163,9 +106,60 @@ function newUpdate() {
 		window.origSetTimeout(newUpdate, 1000);
 	});
 };
+	window.origSetTimeout = window.setTimeout;
+	window.setTimeouts = [];
+	window.setTimeout = function(func, time)
+	{
+	  var fstr = func.toString();
+	  var updater = fstr.match(/window\.setTimeout\(update,\s/i)?true:false;
+	  //console.log('func', updater);
+	
+	  if(!updater)
+	  {
+	    var i = window.origSetTimeout(func, time);
+	    window.setTimeouts.push({
+	      i: i,
+	      func: func,
+	      time: time,
+	    });
+	  }
+	};
+
+window.liveopts_str = "";
+window.groups_str = "";
+window.group_ignore = 0;
+window.timezone = "0";
+window.pretimezone = 0;
+
+if (liveopts_str.length > 0) {
+	var liveopts = $.trim(liveopts_str).split('|');
+	$.each(liveopts, function(index, value) { 
+		var cell = $('tr', '#filter_tbl').children().filter(function(){ return $(this).text() == value; });
+		if (cell) {
+			cell.css('color','white');
+		}
+	});
+} else {
+	var liveopts = new Array();
+}
+
+if (groups_str.length > 0) {
+	var groupopts = $.trim(groups_str).split('|');
+	$.each(groupopts, function(index, value) { 
+		if (value.length > 0) {
+			$('#group_list', '#filter_tbl').append('<option opt="group">'+value+'</option>');
+		}
+	});
+} else {
+	var groupopts = new Array();
+}
+
+if (group_ignore) {
+	$("td[opt='group_ignore']", '#filter_tbl').css('color','white');
+} else {
+	$("td[opt='group_ignore']", '#filter_tbl').css('color','');
+}
 
 console.log('Live Pre Notifier started');
 setTimeout(newUpdate, 400);
-
-
 
